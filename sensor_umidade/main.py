986eb88630df5aea7380fdccf05e2bc997590dc7
LAURA SOFIA LIMA NUNES DA SILVA <24122467@CFP127ALUNO.LOCAL>
2025-08-26 12:18:49 -0300

    Sensor de umidade ok

---
+++ sensor_umidade/main.py
@@ -0,0 +1,30 @@
+from machine import Pin, ADC
+import time
+
+print("Hello, ESP32!")
+
+PINO_SENSOR_UMIDADE = 34
+PINO_LED_SECO = 26
+
+sensor_umidade = ADC(Pin(PINO_SENSOR_UMIDADE))
+led_seco = Pin(PINO_LED_SECO, Pin.OUT)
+
+sensor_umidade.width(ADC.WIDTH_12BIT)
+sensor_umidade.atten(ADC.ATTN_11DB)
+
+while True:
+
+    valor_umidade = sensor_umidade.read()
+
+    print("Valor do sensor:", valor_umidade)
+
+    LIMIAR_SECO = 2000
+
+    if valor_umidade > LIMIAR_SECO:
+        print("ALERTA: Solo seco! É hora de regar.")
+        led_seco.value(1)
+    else:
+        print("Solo úmido. Tudo certo!")
+        led_seco.value(0)
+    
+    time.sleep(5)
